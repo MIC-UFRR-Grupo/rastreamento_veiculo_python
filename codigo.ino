5dec34a2516f0af5cb0beb03ee0b842580e2e139
Erica Camila <ericamila0002@gmail.com>
2025-03-01 20:48:40 -0400

    Código para o ESP32
    Funcionando

---
+++ codigo.ino
@@ -0,0 +1,117 @@
+#include <WiFi.h>
+#include <Firebase_ESP_Client.h>
+#include <TinyGPSPlus.h>
+#include <HardwareSerial.h>
+#include <Wire.h>
+#include <MPU6050.h>
+#include <ambiente.ino>  //#define WIFI_SSID WIFI_PASSWORD
+
+// Objeto Firebase
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+
+// GPS
+HardwareSerial gpsSerial(1);
+TinyGPSPlus gps;
+
+// MPU6050
+MPU6050 mpu;
+bool emMovimento = false;
+
+void setup() {
+  Serial.begin(115200);
+  gpsSerial.begin(9600, SERIAL_8N1, 16, 17);  // RX=16, TX=17 para GPS
+
+  // Configuração do WiFi
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Conectando ao WiFi...");
+  }
+  Serial.println("WiFi conectado!");
+  Serial.println(" v1 ");
+
+  // Configurações do Firebase
+  config.host = FIREBASE_HOST;
+  config.signer.tokens.legacy_token = FIREBASE_AUTH;
+  // Inicializa o Firebase
+  Firebase.begin(&config, &auth);
+  Firebase.reconnectWiFi(true);
+
+  if (!Firebase.ready()) {
+    Serial.println("Falha ao conectar ao Firebase!");
+  } else {
+    Serial.println("Conectado ao Firebase!");
+  }
+
+  // Inicializar MPU6050
+ /*Wire.begin();
+  mpu.initialize();
+  if (!mpu.testConnection()) {
+    Serial.println("Falha ao conectar ao MPU6050");
+    while (1);
+  } else {
+    Serial.println("MPU6050 conectado!");
+  }*/
+}
+
+void loop() {
+  Serial.println("Entrou no loop...");
+  // Capturar dados do GPS
+  while (gpsSerial.available() > 0) {
+    Serial.println("Capturar dados do GPS");
+    gps.encode(gpsSerial.read());
+  }
+
+  if (gps.location.isUpdated()) {
+    Serial.println("if (gps.location.isUpdated())");
+    float latitude = gps.location.lat();
+    float longitude = gps.location.lng();
+    
+    // Detectar movimento com MPU6050
+    /*
+    int16_t ax, ay, az;
+  mpu.getAcceleration(&ax, &ay, &az);
+  emMovimento = (abs(ax) > 5000 || abs(ay) > 5000 || abs(az) > 5000);
+*/
+    // Enviar para o Firebase
+    FirebaseJson json;
+    json.set("latitude", latitude);
+    json.set("longitude", longitude);
+    json.set("em_movimento", emMovimento);
+
+    if (Firebase.RTDB.setJSON(&fbdo, "/vehicles/veiculo5", &json)) {
+      Serial.print("lat ");
+      Serial.print(latitude);
+      Serial.print(" long ");
+      Serial.println(longitude);
+      Serial.println("Dados enviados!");
+    } else {
+      Serial.println(fbdo.errorReason());
+    }
+  }
+/*
+  delay(5000);
+  */
+
+// TESTE 
+/*
+    if (1==1) {
+
+    // Enviar para o Firebase
+    FirebaseJson json;
+    json.set("latitude", 2.8360178);
+    json.set("longitude", -60.709525);
+    json.set("em_movimento", true);
+    json.set("velocidade", 45);
+
+    if (Firebase.RTDB.setJSON(&fbdo, "/vehicles/veiculo5", &json)) {
+      Serial.println("Dados enviados!");
+    } else {
+      Serial.println(fbdo.errorReason());
+    }
+  }*/
+
+  delay(10000);
+}
